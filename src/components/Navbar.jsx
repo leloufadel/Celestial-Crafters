d02f554c9872b314900e676699a62cda53508fbd
leloufadel <leloufadel@gmail.com>
2024-02-14 15:01:59 +0000

    Styled the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddindx} w-full
+    <nav className={`${styles.paddingX} w-full
 items-center py-5 fixed top-0 z-20 bg-primary `}
 >
       <div className="w-full flex justify-between items-center
@@ -18,7 +18,10 @@ window.scrollTo(0, 0);
 }}
 >
 <img src={logo} alt="logo" className="w-9 h- 9 object-contain" />
-<p className="text-white text-[18px] font-bold cursor-pointer ">Lala Aicha <span className='sm:block hidden'>|Portfolio</span></p>
+<p className="text-white text-[18px] 
+font-bold cursor-pointer flex">Lalla &nbsp;
+<span className='sm:block hidden'> | Portfolio</span>
+</p>
 </Link>
 <ul className='list-none hidden sm:flex flex-row gap-10'>
 {navLinks.map((link) => ( 
