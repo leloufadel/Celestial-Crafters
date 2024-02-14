76d24b4868c1d71e863d69b889573c10dcc3f768
leloufadel <leloufadel@gmail.com>
2024-02-14 16:17:28 +0000

    Structure Hero section contents

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+// import { motion } from 'framer-motion';
+// import { styles } from '../styles';
+// import { ComponentCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'> Hero
+    <div className={`${styles.paddingX}
+    absolute insert-0 top-[120px] max-w-7xl mx-auto 
+    flex flex-row items-start gap-5
+    `}>
+
+    </div>
+    
+    </section>
   )
 }
 
