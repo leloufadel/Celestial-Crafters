33e3d9870772b6066c9067caf9c995495113fbe9
leloufadel <leloufadel@gmail.com>
2024-02-14 16:38:17 +0000

    Add a rounded-full circle with a gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { motion } from 'framer-motion';
-// import { styles } from '../styles';
+import { styles } from '../styles';
 // import { ComponentCanvas } from './canvas';
 
 const Hero = () => {
@@ -8,8 +8,16 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'> Hero
     <div className={`${styles.paddingX}
     absolute insert-0 top-[120px] max-w-7xl mx-auto 
-    flex flex-row items-start gap-5
-    `}>
+    flex flex-row items-start gap-5`}>
+      <div className="flex flex-col justify-center
+      items-centre mt-5 ">
+        <div className='w-5 h-5 rounded-full bg-[#915eff]'>
+        <div className='w-1 sm:h-80 h-40 violet-gradient ml-2 mt-2'>
+
+        </div>
+        </div>
+
+      </div>
 
     </div>
     
