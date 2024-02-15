6ea8a5061df5f0a2d0349aeda56b20206e97b345
leloufadel <leloufadel@gmail.com>
2024-02-15 14:23:03 +0000

    Update the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
@@ -11,11 +11,8 @@ const Hero = () => {
     flex flex-row items-start gap-5`}>
       <div className="flex flex-col justify-center
       items-centre mt-5 ">
-        <div className='w-5 h-5 rounded-full bg-[#915eff]'>
-        <div className='w-1 sm:h-80 h-40 violet-gradient ml-2 mt-5'>
-
-        </div>
-       
+        <div className='w-5 h-5 rounded-full bg-[#915eff]'></div>
+        <div className='w-1 sm:h-80 h-40 violet-gradient ml-2 '></div>
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} 
@@ -27,12 +24,16 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 ml-6 text-white-100`}>
           A full-stack developer, I develop  <br className='sm:block hidden'/> seamless web applications 
           </p>
-        </div>
+
       </div>
+
     </div>
+
+    
     <ComputersCanvas />
 
     </section>
+    
   )
 }
 
