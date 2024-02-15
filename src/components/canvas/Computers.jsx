5763075413a7051a60f17adf38e50e454dc8cab5
leloufadel <leloufadel@gmail.com>
2024-02-15 11:37:20 +0000

    add a computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,8 +10,30 @@ const Computers = () => {
     <mesh>
       < hemisphereLight intensity={0.15} 
       groundcolor = "black"/>
+      <pointLight intensity={1} />
+      <spotLight position={[-20, 50, 10]}
+      angle={0.14}
+      />
+      <primitive object={computer.scene} 
+      scale ={0.75}
+      position = {[0, -3/25, -1.5]}
+      rotation = {[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
 
+const ComputersCanvas = () => {
+return (
+  <Canvas
+  frameloop='demand'
+  shadows
+  camera={{position: [20, 3, 5], fov:25}}
+  gl={{preserveDrawingBuffer: true}}
+  >
+
+  </Canvas>
+
+)
+}
 export default Computers
