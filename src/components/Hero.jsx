bc8f90b955b095457338df925f5e5d442d2c3b4b
leloufadel <leloufadel@gmail.com>
2024-02-19 15:58:16 +0000

    Adjusted the positioning of the button in the Hero component to move it down slightly for better alignment.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
   <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
     
 
-    {/* <motion.div
+    <motion.div
       animate={{
         y: [0, 24, 0],
       }}
@@ -49,7 +49,7 @@ const Hero = () => {
         repeatType: "loop",
       }}
       className='w-3 h-3 rounded-full bg-secondary mb-1'
-    /> */}
+    />
   </div>
 </a>
 </div>
