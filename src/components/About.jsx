c49b888631773e9993f5a7dacab176b630909c7c
leloufadel <leloufadel@gmail.com>
2024-02-20 11:48:09 +0000

    update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-// import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
   const ServiceCard = ({ index, title, icon }) => (
@@ -63,4 +63,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
