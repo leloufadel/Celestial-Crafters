8d3dcd3df6aa42d88be4c0697faa55bb79288671
leloufadel <leloufadel@gmail.com>
2024-02-29 15:34:43 +0000

    Add subText

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {verticalTimeline, VerticalTimelineElement } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
+import { motion } from 'framer-motion';
 
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant} from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+   <>
+   <motion.div variants={textVariant()} >
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+
+   </>
   )
 }
 
