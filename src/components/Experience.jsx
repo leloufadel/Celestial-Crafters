38205b4e33ab889eed2b77fc3d88bcaee2acd3c1
leloufadel <leloufadel@gmail.com>
2024-02-29 15:10:17 +0000

    intial commit experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import {verticalTimeline, VerticalTimelineElement } from 'framer-motion'
+import 'react-vertical-timeline-component/style.min.css'
+
 
 const Experience = () => {
   return (
