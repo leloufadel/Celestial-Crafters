75d0631fb227db5a3ea3b5b9185d9db68f4cce7f
leloufadel <leloufadel@gmail.com>
2024-02-29 17:10:00 +0000

    Add work experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {verticalTimeline, VerticalTimelineElement } from 'framer-motion'
+// import {verticalTimeline, VerticalTimelineElement } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-import { experiences } from '../constants';
+// import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant} from '../utils/motion';
 
@@ -12,13 +12,12 @@ const Experience = () => {
   return (
    <>
    <motion.div variants={textVariant()} >
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview.</h2>
+      <p className={styles.sectionSubText}>What I have done so far</p>
+      <h2 className={styles.sectionHeadText}>Work Experience.</h2>
     </motion.div>
-
 
    </>
   )
 }
 
-export default Experience
+export default SectionWrapper(Experience, "work")
