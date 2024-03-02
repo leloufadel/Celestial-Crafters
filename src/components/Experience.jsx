d847a0e8f031931337e9c1edf8a12ab69b118492
leloufadel <leloufadel@gmail.com>
2024-03-02 07:18:42 +0000

    Update experience Card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import {verticalTimeline, VerticalTimelineElement } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 import { motion } from 'framer-motion';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
@@ -7,19 +6,29 @@ import { styles } from '../styles';
 import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant} from '../utils/motion';
-// import { VerticalTimeline } from 'react-vertical-timeline-component';
 
-const ExperienceCard = ({ experience }) => {
+const ExperienceCard = ({ experience }) => (
  <VerticalTimelineElement 
  contentStyle={{ background: '#1d1836', color: 
  '#fff' }}
  contentArrowStyle ={{ borderRight: '7px solid #2326131'}}
  date={experience.date}
  iconStyle={{ background: experience.iconBg}}
+icon={
+  <div>
+    <img src={experience.icon} 
+    alt={experience.company_name}
+    className="w-[60%] h-[60%]
+    object-contain"
+    />
+  
+  </div>
+}
+
  >
 
  </VerticalTimelineElement>
-}
+)
 
 const Experience = () => {
   return (
