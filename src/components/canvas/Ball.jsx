4f71ccd263d83b7ed1400a282ee01d029dd8a6f9
leloufadel <leloufadel@gmail.com>
2024-03-03 11:51:03 +0000

    Add BallCanvas component with OrbitControls and Preload

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Ball = () => {
+  const [decal] = useTexture([ props.imgUrl]);
   return (
     <div>Ball</div>
   )
-}
+};
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
 
 export default Ball
