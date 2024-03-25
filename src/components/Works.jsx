f3f1db5a165e3ab878f802282118b04d6230ee84
leloufadel <leloufadel@gmail.com>
2024-03-25 21:37:13 +0000

    intinial importations

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import {motion} from 'framer-motion';
+import { styles } from '../styles';
+import {github} from '../assests';
+import { Sectionwapper } from '../hoc';
+import {projects} from '../constants';
+import { fadeIn, textVariant} from '../utils/motion'; 
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    
+    </>
   )
 }
 
